b2530beb8525c774926be3f6b6bbd561a73f8593
Simone Caronni <negativo17@gmail.com>
2020-03-25 23:10:22 +0100

    Do not hide the ckey

--- wiiu_decrypt.py
+++ wiiu_decrypt.py
@@ -17,16 +17,8 @@ import sys
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 
-# put the common key here to decrypt things
-wiiu_common_key = 'D7B00402659BA2ABD2CB0DB27FA2B656'
-
-##########################
-
-wiiu_common_key_hash = hashlib.sha1(wiiu_common_key.encode('utf-8').upper())
-if wiiu_common_key_hash.hexdigest() != 'e3fbc19d1306f6243afe852ab35ed9e1e4777d3a':
-    sys.exit('Wrong Wii U Common Key. Place the correct one in the script.')
-
-ckey = binascii.unhexlify(wiiu_common_key)
+# Wii U common key to decrypt things
+ckey = binascii.unhexlify('D7B00402659BA2ABD2CB0DB27FA2B656')
 
 readsize = 8 * 1024 * 1024
 
